0dd968658053b7ae767f093badd348a58fd83be5
tak.song-at-585251656769 <>
2023-10-06 18:49:21 +0900

    기본 세팅

---
+++ src/api.js
@@ -0,0 +1,22 @@
+// api.js
+import axios from "axios";
+
+const OPENAI_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions";
+
+export const getResponseFromGPT = async (prompt) => {
+  const response = await axios.post(
+    OPENAI_ENDPOINT,
+    {
+      prompt: prompt,
+      max_tokens: 150,
+    },
+    {
+      headers: {
+        Authorization: `Bearer YOUR_OPENAI_API_KEY`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return response.data.choices[0].text.trim();
+};
