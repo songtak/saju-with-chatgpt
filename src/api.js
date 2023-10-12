24cfe9a2bf06cef842c33f7991b467647413206d
tak.song-at-585251656769 <>
2023-10-12 13:04:09 +0900

    수정

--- src/api.js
+++ src/api.js
@@ -8,10 +8,6 @@ const OPENAI_ENDPOINT = "https://api.openai.com/v1/chat/completions";
 //   "https://api.openai.com/v1/models/gpt-3.5-turbo-instruct";
 
 export const getResponseFromGPT = async (prompt) => {
-  console.log(
-    "process.env.REACT_APP_OPENAI_API_KEY!!",
-    process.env.REACT_APP_OPENAI_API_KEY
-  );
   const response = await axios.post(
     OPENAI_ENDPOINT,
     {
